ebe153acc9ab100dffa72a71ca3c36983cfbdf83
andreaquilau <andre.aquilau@outlook.com>
2023-10-22 17:22:58 -0400

    first commit

--- Program.cs
+++ Program.cs
@@ -17,15 +17,28 @@ using StreamWriter streamWriter = new StreamWriter("./text.txt");
 
 foreach (string text in texts)
 {
-    // Define a expressão regular para remover os caracteres unicode e ANSI
-    string unescaped = Regex.Unescape(text);
-    Regex regex = new Regex(@"[^\x00-\x7F]|\u001b\[\d{1,2}m|[\x80-\xFF]");
 
-    // Remove os caracteres unicode e ANSI da string de entrada
-    string output = regex.Replace(unescaped, string.Empty).Trim();
+    CleanString cleanString = new CleanString();
+
+    var output = cleanString.RemoveUnicodeANSI(text);
 
     Console.WriteLine(output);
 
     streamWriter.WriteLine(output);
 
+}
+
+public class CleanString
+{
+    public string RemoveUnicodeANSI(string input)
+    {
+        // Define a expressão regular para remover os caracteres unicode e ANSI
+        string unescaped = Regex.Unescape(input);
+        Regex regex = new Regex(@"[^\x00-\x7F]|\u001b\[\d{1,2}m|[\x80-\xFF]");
+
+        // Remove os caracteres unicode e ANSI da string de entrada
+        string output = regex.Replace(unescaped, string.Empty).Trim();
+
+        return output;
+    }
 }
